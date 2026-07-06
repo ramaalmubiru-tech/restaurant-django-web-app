1d33d550b1038f4efa7f44f36a11014c6f192caf
ramaal <ramaalmubiru@gmail.com>
2026-07-06 09:37:58 +0300

    second commit

--- menu/views.py
+++ menu/views.py
@@ -5,3 +5,11 @@ from .models import Category, MenuItem, DrinkType, Drink
 def menu_list(request):
     categories= Category.objects.prefetch_related('menuitem_set').all()
     drink_types=DrinkType.objects.prefetch_related('drink_set').all()
+
+
+    context={
+        'categories': categories,
+        'drink_types': drink_types,
+    }
+    return render(request, 'menu/menu_list.html', context)
+
