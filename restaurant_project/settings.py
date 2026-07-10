37dc98b128a7f371672d6a5c8fcad69779cfaee0
ramaal <ramaalmubiru@gmail.com>
2026-07-11 01:08:43 +0300

    update2

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -81,14 +81,20 @@ WSGI_APPLICATION = 'restaurant_project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/6.0/ref/settings/#database
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR} /db.sqlite3',
-        conn_max_age=600,  
-    )
-}
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
